c08f6f0ad397141a1a56a748069b2cb42d7894a6
DoodPop <hypergon101@gmail.com>
2024-06-11 19:35:25 -0600

    connecting

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const { Client } = require('pg');
+const bodyParser = require('body-parser');
+
 
 const port = process.env.PORT || 3000;
 
